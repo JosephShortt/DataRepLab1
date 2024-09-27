df21aafdfe7ba2f13769a0f72ad786ac3a34a3ab
JosephShortt <josephshortt1@gmail.com>
2024-09-27 08:32:52 +0000

    2a finished

---
+++ 2a.js
@@ -0,0 +1,17 @@
+/*
+const x =function Hello(){
+    console.log("Hello");
+}
+x();
+
+let y = ()=>{
+    console.log("Bye");
+}
+y();
+*/
+
+//Arrow function that returns Data Representation & Querying
+const arrow = ()=>{
+    console.log("Data Representation & Querying");
+}
+arrow();
