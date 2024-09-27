2484097382eade892f86bdbdd5be8ad73b08cdaa
JosephShortt <josephshortt1@gmail.com>
2024-09-27 09:02:35 +0000

    2d completed

---
+++ 2d.js
@@ -0,0 +1,10 @@
+const ages = [25, 31, 42, 77];
+//.map function that returns all array items that are greater than 70 
+//Multiplies by 2
+let d = ages.map((item)=> {
+    if(item<70){
+        return item*2;
+    }
+})
+
+console.group(d);
